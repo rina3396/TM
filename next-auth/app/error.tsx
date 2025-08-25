ff8c5703158f8eb883612cb64542fba408dd7067
{rina3396} <{enjori.age@gmail.com}>
2025-08-26 04:16:58 +0900

    v2

---
+++ next-auth/app/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+//エラー画面
+const Error = () => {
+    return (
+        <>
+            <div className="text-center text-5xl font-bold mb-3">500</div>
+            <div className="text-center text-xl font-bold">Error</div>
+        </>
+    )
+}
+
+export default Error
---
+++ next-auth/app/not-found.tsx
@@ -0,0 +1,11 @@
+//404画面
+const notFound = ()=>{
+    return(
+        <>
+            <div className="text-center text-5xl font-bold mb-3">404</div>
+            <div className="text-center text-xl font-bold">Not Found</div>
+        </>
+    )
+}
+
+export default notFound
