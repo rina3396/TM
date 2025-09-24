55b332b4a5a9ec9075e3d2aa9eec176628e1620f
rina3396 <enjori.age@gmail.com>
2025-09-25 02:24:12 +0900

    v3-1

---
+++ travel-app/lib/supabase/client.ts
@@ -0,0 +1,9 @@
+// lib/supabase/client.ts
+import { createClient } from "@supabase/supabase-js"
+
+export function createClientBrowser() {
+    return createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+}
